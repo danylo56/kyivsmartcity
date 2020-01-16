23d1a1893b6d2608aec92f3e2df4b25e713f494a
Sergey Denysov <denisov.univ@gmail.com>
2020-01-16 22:52:14 +0200

    Some refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -7,22 +7,51 @@ function log(){
 	localStorage.setItem(text + "_name", name);
 }
 
-function enter(){
-	var text = document.getElementById('text_2').value;
-	var password = document.getElementById('password_2').value;
+function enter(e){
+	e.preventDefault(); // Stop for auto-submit
 
-	if (localStorage.getItem(text + '_password') == password){
-		alert('Привіт, ' + localStorage.getItem(text + '_name'));
-	}else{
-		alert("Ти хто? Вийди отсюда розбійник.");
+	const login = document.getElementById('text_2').value;
+	const password = document.getElementById('password_2').value;
+
+	console.log(login);
+
+	const savedUserString = localStorage.getItem(login);
+	if(savedUserString) {
+		const savedUser = JSON.parse(savedUserString);
+		console.log(savedUser);
+
+		if(savedUser && savedUser.password == password) {
+			alert('Привіт, ' + savedUser.fullName);
+			return;
+		}
 	}
+
+	alert("Ти хто? Вийди отсюда розбійник.");
 }
 
 function show1(){
 	//console.log('Function works')
-	document.getElementById('logon').setAttribute('style', 'height : 50vh');
+	document.getElementById('registrationForm').style.display = 'flex';
+	document.getElementById('loginForm').style.display = 'none';
 }
 function show2(){
 	//console.log('Function works')
-	document.getElementById('login').setAttribute('style', 'height : 50vh');
+	document.getElementById('registrationForm').style.display = 'none';
+	document.getElementById('loginForm').style.display = 'flex';
+}
+
+function getUserFromRegistrationFields() {
+	return {
+		login: document.getElementById('login').value,
+		fullName: document.getElementById('fullName').value,
+		password: document.getElementById('password').value
+	};
+}
+
+function register(e) {
+	e.preventDefault(); // Stop for auto-submit
+	const user = getUserFromRegistrationFields();
+	localStorage.setItem(user.login, JSON.stringify(user));
+
+	console.log('User registered:', user);
 }
